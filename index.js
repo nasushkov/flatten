8ca4b0465a9bd14e21cbe2653cd6d920c85a5713
Nikita Sushkov <nasushkov@gmail.com>
2016-08-28 10:20:12 +0300

    added comments and gitignore

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+/**
+ * Flatten function implementation
+ * @param {Array} arr - array to flatten
+ * @param {Array} result - array to aggregate result (the same array passed to each recursive call)
+ * @returns {Array}
+ */
 const flatten = (arr, result = []) => {
   if (!Array.isArray(arr)) {
     throw new TypeError('flatten accepts only array as an argument')
@@ -12,4 +18,8 @@ const flatten = (arr, result = []) => {
   return result;
 }
 
+/**
+ * @module flatten
+ * @type {function()}
+ */
 module.exports = flatten
